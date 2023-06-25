337973293752e044382a18d44023057c2e794c54
lauriciodev <developerwebmasterpro@gmail.com>
2023-06-25 13:49:13 -0300

    reações

--- src/routes.ts
+++ src/routes.ts
@@ -4,9 +4,14 @@ import { RoomController } from "./controllers/RoomController";
 
 const routes = Router();
 
+// subjects
 routes.post("/subject", new SubjectController().create);
 
-//room
+// room
 routes.post("/room", new RoomController().create);
+routes.post("/room/:idRoom/create", new RoomController().createVideo);
+routes.post("/room/:idRoom/subject", new RoomController().roomSubject);
+//listage,
+routes.get("/room", new RoomController().index);
 
 export default routes;
